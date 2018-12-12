080bf1f129cf122b55bbb106163a95436f396430
13072009 <shur2005@gmail.com>
2018-12-12 23:17:56 +0100

    V9

--- app/src/main/java/com/hfad/tabletrainer/HomeFragment.java
+++ app/src/main/java/com/hfad/tabletrainer/HomeFragment.java
@@ -35,7 +35,7 @@ public class HomeFragment extends Fragment {
 
 
         View v = inflater.inflate(R.layout.fragment_home, container, false);
-        final Integer[] minuterArr = {1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60};
+        final Integer[] minuterArr = { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60};
         final Integer[] tableArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         textViewTime =  this.getActivity().findViewById(R.id.timerView);
         //The spinner is defined in our xml file
